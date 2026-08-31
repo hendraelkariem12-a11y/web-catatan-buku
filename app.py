50c674c43497ea60676ff0e4e2a17011ae23728d
hendraelkariem12-a11y <hendraelkariem12@gmail.com>
2026-08-31 13:48:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1418,4 +1418,6 @@ def restore_db():
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
